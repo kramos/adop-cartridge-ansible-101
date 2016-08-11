726d254a396c8a95e3d701c13fedd7d2a91e44f8
Markos Rendell <markosrendell@gmail.com>
2016-08-11 10:09:30 +0100

    Initial 1 job

---
+++ jenkins/jobs/dsl/ansible_101_jobs.groovy
@@ -0,0 +1,186 @@
+// Folders
+def workspaceFolderName = "${WORKSPACE_NAME}"
+def projectFolderName = "${PROJECT_NAME}"
+
+// Variables
+def referenceAppGitRepo = "todo"
+def referenceAppGitUrl = "ssh://jenkins@gerrit:29418/${PROJECT_NAME}/" + referenceAppGitRepo
+
+// Jobs
+def installAnsible = freeStyleJob(projectFolderName + "/1_Install_Ansible")
+def runAdhocCommands = freeStyleJob(projectFolderName + "/2_Run_Adhoc_Commands")
+
+// Views
+def pipelineView = buildPipelineView(projectFolderName + "/Example_Alexia_Pipeline")
+
+//pipelineView.with{
+//    title('Example Alexia Pipeline')
+//    displayedBuilds(5)
+//    selectedJob(projectFolderName + "/Get_Code")
+//    showPipelineParameters()
+//    showPipelineDefinitionHeader()
+//    refreshFrequency(5)
+//}
+//
+installAnsible.with{
+  def desc = "This job installs Ansible (in a Docker container for the sake of doing it somewhere clean) using Pip.  For more information see: http://docs.ansible.com/ansible/intro_installation.html"
+  description(desc)
+  wrappers {
+    preBuildCleanup()
+    injectPasswords()
+    maskPasswords()
+    sshAgent("adop-jenkins-master")
+  }
+  environmentVariables {
+      env('WORKSPACE_NAME',workspaceFolderName)
+      env('PROJECT_NAME',projectFolderName)
+  }
+  label("docker")
+  steps {
+    shell('''set -xe
+            |echo  '''.stripMargin()) + desc + '''
+            |
+            |cat <<EOF > Dockerfile
+            |FROM centos:latest
+            |RUN yum install -y epel-release && yum install -y ansible
+            |EOF
+            |
+            |docker build -t ansible${BUILD_NUMBER} .
+            |printf "\n\nLet's now test the Ansible install\n\n"
+            |docker run --rm -t ansible${BUILD_NUMBER} ansible --version
+            |printf "\\n\\nThat looked good!\n\nLet's now just remove our temporary image"
+            |docker rmi -f ansible${BUILD_NUMBER}
+            |'''.stripMargin())
+  }
+  publishers{
+    downstreamParameterized{
+      trigger(projectFolderName + "/Install"){
+        condition("UNSTABLE_OR_BETTER")
+        parameters{
+          predefinedProp("B",'${BUILD_NUMBER}')
+          predefinedProp("PARENT_BUILD",'${JOB_NAME}')
+        }
+      }
+    }
+  }
+}
+
+install.with{
+  description("This job performs an npm install")
+  parameters{
+    stringParam("B",'',"Parent build number")
+    stringParam("PARENT_BUILD","Get_Code","Parent build name")
+  }
+  environmentVariables {
+      env('WORKSPACE_NAME',workspaceFolderName)
+      env('PROJECT_NAME',projectFolderName)
+  }
+  wrappers {
+    preBuildCleanup()
+    injectPasswords()
+    maskPasswords()
+    sshAgent("adop-jenkins-master")
+  }
+  label("docker")
+  steps {
+    shell('''set -x
+            |echo Run an install 
+            |
+            |docker run \\
+            |		--rm \\
+            |		-v /var/run/docker.sock:/var/run/docker.sock \\
+            |		-v jenkins_slave_home:/jenkins_slave_home/ \\
+            |		--workdir /jenkins_slave_home/${PROJECT_NAME}/Get_Code \\
+            |		node \\
+            |		npm install --save	
+            |'''.stripMargin())
+  }
+  publishers{
+    downstreamParameterized{
+      trigger(projectFolderName + "/Test"){
+        condition("UNSTABLE_OR_BETTER")
+        parameters{
+          predefinedProp("B",'${B}')
+          predefinedProp("PARENT_BUILD", '${JOB_NAME}')
+        }
+      }
+    }
+  }
+}
+
+test.with{
+  description("When triggered this will run the tests.")
+  parameters{
+    stringParam("B",'',"Parent build number")
+    stringParam("PARENT_BUILD","Get_Code","Parent build name")
+  }
+  wrappers {
+    preBuildCleanup()
+    injectPasswords()
+    maskPasswords()
+    sshAgent("adop-jenkins-master")
+  }
+  environmentVariables {
+      env('WORKSPACE_NAME',workspaceFolderName)
+      env('PROJECT_NAME',projectFolderName)
+  }
+  label("docker")
+  steps {
+    shell('''set -x
+            |echo Run unit tests
+            |
+            |docker run \\
+            |		--rm \\
+            |		-v /var/run/docker.sock:/var/run/docker.sock \\
+            |		-v jenkins_slave_home:/jenkins_slave_home/ \\
+            |		--workdir /jenkins_slave_home/${PROJECT_NAME}/Get_Code \\
+            |		node \\
+            |		npm run test
+            |'''.stripMargin())
+  }
+  publishers{
+    downstreamParameterized{
+      trigger(projectFolderName + "/Lint"){
+        condition("UNSTABLE_OR_BETTER")
+        parameters{
+          predefinedProp("B",'${BUILD_NUMBER}')
+          predefinedProp("PARENT_BUILD", '${JOB_NAME}')
+        }
+      }
+    }
+  }
+}
+
+lint.with{
+  description("This job will perform static code analysis")
+  parameters{
+    stringParam("B",'',"Parent build number")
+    stringParam("PARENT_BUILD","Get_Code","Parent build name")
+  }
+  environmentVariables {
+      env('WORKSPACE_NAME',workspaceFolderName)
+      env('PROJECT_NAME',projectFolderName)
+  }
+  wrappers {
+    preBuildCleanup()
+    injectPasswords()
+    maskPasswords()
+    sshAgent("adop-jenkins-master")
+  }
+  label("docker")
+  steps {
+    shell('''set -x
+            |echo Run static code analysis 
+            |
+            |docker run \\
+            |		--rm \\
+            |		-v /var/run/docker.sock:/var/run/docker.sock \\
+            |		-v jenkins_slave_home:/jenkins_slave_home/ \\
+            |		--workdir /jenkins_slave_home/${PROJECT_NAME}/Get_Code \\
+            |		node \\
+            |		npm run lint
+            |'''.stripMargin())
+  }
+}
+
+
